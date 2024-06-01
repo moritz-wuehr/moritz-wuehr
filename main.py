50f211ee2d2e35e7f35eb4674350b63c1e8d149e
moritz-wuehr <83395059+moritz-wuehr@users.noreply.github.com>
2024-06-01 18:42:00 +0200

    update

--- main.py
+++ main.py
@@ -1,20 +1,19 @@
-import requests
-from pyscript import document
 
 
-url = "https://apis.deutschebahn.com/db/apis/ris-boards/v1/public/arrivals/8500010?timeStart=2024-05-29T14%3A07%3A54.140Z&timeEnd=2024-05-29T15%3A57%3A38.866Z&includeStationGroup=true&includeMessagesDisruptions=true"
+#import requests
 
-headers = {
-    "DB-Client-Id": "df6ed5c4704419a5da56e38441dcdd62",
-    "DB-Api-Key": "5c0f29d6832f31193c822e3cafc2a087",
-    "accept": "application/vnd.de.db.ris+json"
-}
+#url = "https://apis.deutschebahn.com/db/apis/ris-boards/v1/public/arrivals/8500010?timeStart=2024-05-29T14%3A07%3A54.140Z&timeEnd=2024-05-29T15%3A57%3A38.866Z&includeStationGroup=true&includeMessagesDisruptions=true"
 
-response = requests.get(url, headers=headers)
+#headers = {
+#    "DB-Client-Id": "df6ed5c4704419a5da56e38441dcdd62",
+#    "DB-Api-Key": "5c0f29d6832f31193c822e3cafc2a087",
+#    "accept": "application/vnd.de.db.ris+json"
+#}
 
-print(response.text)
+#response = requests.get(url, headers=headers)
+
+#print(response.text)
+
+print("hello")
 
 
-def return(event):
-    output_div = document.querySelector("#output")
-    output_div.innerText = "hallo"
